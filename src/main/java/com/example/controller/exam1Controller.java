27599b53db9f2a089a2d47fd8a22e8229e817263
matsunagayuki <yuki.matsunaga@rakus-partners.co.jp>
2021-11-01 17:29:03 +0900

    beginner

---
+++ src/main/java/com/example/controller/exam1Controller.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex1")
+public class exam1Controller {
+	@Autowired
+	private HttpSession session;
+	@RequestMapping("")
+	public String index() {
+	return "exam01";
+	}
+	@RequestMapping("/exam-01")
+	public String ex1(String name) {
+		session.setAttribute("name", name);
+		return "exam-01";
+	}
+}
